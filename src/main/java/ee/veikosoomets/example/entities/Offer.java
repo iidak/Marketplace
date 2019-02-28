83621995d36c8983bcb8ef37b9bad0ec9dd60446
Stenxz <stearm@ttu.ee>
2019-02-28 12:28:56 +0200

    Sten - lisasin igast klasse juurde vol2

---
+++ src/main/java/ee/veikosoomets/example/entities/Offer.java
@@ -0,0 +1,38 @@
+package ee.veikosoomets.example.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Offer {
+
+    Campaign campaign;
+    Influencer influencer;
+    enum Status { SENT, ACCEPTED, MATCHED, DECLINED }
+    Status status;
+
+    public Offer(Campaign campaign, Influencer influencer) {
+        this.campaign = campaign;
+        this.influencer = influencer;
+        this.status = Status.SENT;
+    }
+
+    public void influencerAccepted() {
+        status = Status.ACCEPTED;
+    }
+
+    public void brandAccepted() {
+        status = Status.MATCHED;
+    }
+
+    public void offerDeclined() {
+        status = Status.DECLINED;
+    }
+    
+}
