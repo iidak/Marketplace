f480129efb1f22e24a0b2548151c93b4c04d8234
Stenxz <stearm@ttu.ee>
2019-02-28 12:03:57 +0200

    Sten - lisasin igast klasse juurde

---
+++ src/main/java/ee/veikosoomets/example/entities/Brand.java
@@ -0,0 +1,21 @@
+package ee.veikosoomets.example.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Brand extends User {
+
+    private String businessName;
+
+    public Brand(String email, String password, String firstName, String lastName, String businessName) {
+        super(email, password, firstName, lastName);
+        this.businessName = businessName;
+    }
+}
